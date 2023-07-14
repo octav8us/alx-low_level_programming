3d3987f70cc2c62ae1e205b0ea4153da1bc90873
octav8us <octav8us@gmail.com>
2023-07-14 17:12:27 +0300

    make code more bretty

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 /**
  * main - prints random number and it's type
  *
- *Return: Always 0 
+ *Return: Always 0
  *
 */
 int main(void)
@@ -13,11 +13,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0)
+	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 	else
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	return (0);
 }
