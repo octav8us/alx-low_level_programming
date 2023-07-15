e38161011b1f8cb0c521ea8f10e64936d3329b47
octav8us <octav8us@gmail.com>
2023-07-15 04:18:43 +0300

    i guess i get it

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ *  main - prints the alphapet
+ *
+ *  Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+
+	for (i = 0; i <  10; i++)
+	{
+		putchar (i + '0');
+
+		putchar (',');
+		putchar (' ');
+
+	}
+
+	putchar ('\n');
+	return (0);
+}
