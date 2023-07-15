86d9bb763a7994cc62cdf1deba4e2bf2ea519964
octav8us <octav8us@gmail.com>
2023-07-15 04:07:40 +0300

    this is getting weird

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ *  main - prints numbers of base 16
+ *
+ *  Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+
+	for (i = 0; i <  10; i++)
+		putchar ('0' + i);
+
+	for (i = 'a'; i <=  'f'; i++)
+		putchar ('0' + i);
+
+
+
+	putchar ('\n');
+	return (0);
+}
