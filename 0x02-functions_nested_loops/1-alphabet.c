e43b5ae525636f40df268418f79eabdf2d8cc833
octav8us <octav8us@gmail.com>
2023-07-18 17:41:41 +0300

    print every letter every time

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * main - prints the whole alphapet
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char i;
+
+	for (char i = 'a';  i <= 'z' ; i++)
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+	return (0);
+}
