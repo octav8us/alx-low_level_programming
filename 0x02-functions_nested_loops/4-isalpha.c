54e9e34fece2e6b582c30ceae02426ea79d8bba3
octav8us <octav8us@gmail.com>
2023-07-18 18:45:43 +0300

    are you really who you are?

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - checks if @c is a charachter or not
+ * @c: the input to be checked
+ *
+ * Return: 1 if the passed is char , 0 if not
+ */
+
+int _isalpha(int c)
+{
+
+	if (c > 'a' && c < 'z' ||
+	    c > 'A' && c < 'Z')
+		return (1);
+	else
+		return (0);
+
+
+}
