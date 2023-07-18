9d3b6235de0b7ee445b914f11d259d64825fdc8d
octav8us <octav8us@gmail.com>
2023-07-18 20:54:18 +0300

    hotfix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,16 @@ void print_to_98(int n)
 		return;
 	else if (n < 98)
 	{
-		for (n ; n <= 98 ; n++)
+		while (n <= 98)
 		{
-		printf(" ,%d", n);
+		printf(" ,%d", n++);
 		}
 	}
 	else
 	{
-		for (n ; n >= 98 ; n--)
+		while ( n >= 98 )
 		{
-		printf(" ,%d", n);
+		printf(" ,%d", n--);
 		}
 	}
 }
