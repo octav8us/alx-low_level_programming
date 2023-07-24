7c969fde52a67ed9f9f09c6a4d42be55dc6b752b
octav8us <octav8us@gmail.com>
2023-07-24 22:58:12 +0300

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * reset_to_98 - takes a pointer and changes the value it 
+ * potints to to 98.
+ * @n: the pointer
+ * Return: void
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ALX lesson about arrays and pointers
