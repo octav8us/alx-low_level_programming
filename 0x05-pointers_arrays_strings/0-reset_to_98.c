3e78ccc218a3a1181c41320d496f0a8b04c82885
octav8us <octav8us@gmail.com>
2023-07-24 23:40:24 +0300

    betty task 0 And task 1

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include"main.h"
 /**
- * reset_to_98 - takes a pointer and changes the value it 
+ * reset_to_98 - takes a pointer and changes the value it
  * potints to to 98.
  * @n: the pointer
  * Return: void
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps two numbers
+ * @a: first number pointer
+ * @b: second number pointer
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temporary;
+
+	temporary = *a;
+	*a = *b;
+	*b = temporary;
+
+
+
+}
