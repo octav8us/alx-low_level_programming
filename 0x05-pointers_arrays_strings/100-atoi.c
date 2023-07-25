fbc0c9acf3bd0f94b714a319984818dfa717d9f5
octav8us <octav8us@gmail.com>
2023-07-25 20:50:27 +0300

    task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *_atoi - extract the nuber from intgeger
+ * @s: the string in question
+ * Return: the signed number
+ */
+
+int _atoi(char *s)
+{
+	int i;
+	int j;
+	int result = 0;
+	int negative = 0;
+
+	for (i = 0; !(('0' <= s[i]) && (s[i] <= '9')); i++)
+	{
+	if (s[i] == '-')
+		negative = !negative;
+
+	}
+
+	for (j = 0; ('0' <= s[i]) && (s[i] <= '9'); i++, j++)
+	{
+		result = (result * 10) + (s[i] - '0');
+	}
+
+
+	if (negative)
+		result *= -1;
+	return (result);
+}
