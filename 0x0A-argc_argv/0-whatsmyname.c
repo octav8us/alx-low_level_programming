0c352669f79f3167b53d547ea9110b6f051c99b2
octav8us <octav8us@gmail.com>
2023-08-04 22:15:54 +0300

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print program name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if sucess
+ */
+
+int main (int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+Tasks explain argc, argv
