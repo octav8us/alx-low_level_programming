a0d9e4981796465ec85e7ddc723f5cbba8f852cf
octav8us <octav8us@gmail.com>
2023-08-07 22:52:49 +0300

    hotfix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,7 +13,13 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	p = (char *) malloc(sizeof(c) * size);
+
+	if (p == NULL)
+		return (0);
 
 	for (i = 0; i < size; i++)
 		p[i] = c;
