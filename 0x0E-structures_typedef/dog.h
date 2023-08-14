3b3fc52a36249aadbd43ac86bbc44306e10d555d
octav8us <octav8us@gmail.com>
2023-08-14 19:16:17 +0300

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Basic dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+#endif
