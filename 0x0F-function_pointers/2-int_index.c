b61875352fbde6c2038d0f84228df47b229644eb
octav8us <octav8us@gmail.com>
2023-08-16 18:00:31 +0300

    task 2 hotfix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,7 +14,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || (!array) || (!cmp))
 		return (-1);
 
 	for (i = 0; i < (size - 1); i++)
@@ -25,5 +25,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 	}
 
 
-	return (0);
+	return (-1);
 }
