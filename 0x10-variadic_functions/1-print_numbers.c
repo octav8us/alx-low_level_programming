5e1452a5408a1702122745212e9d271602a16db1
octav8us <octav8us@gmail.com>
2023-08-18 03:47:17 +0300

    bettyfix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,10 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 1; i < n ; i++)
 	{
-		if (!separator )
+		if (!separator)
 			printf("%d", va_arg(ap, int));
 		else
-			printf("%s%d", separator , va_arg(ap, int));
+			printf("%s%d", separator, va_arg(ap, int));
 
 
 
