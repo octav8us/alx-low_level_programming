ad86fc560610015bc079b8ad2dbd136ce077e121
octav8us <octav8us@gmail.com>
2023-08-18 03:03:56 +0300

    hotfix

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,17 +13,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	if (separator == NULL)
+	if (!separator)
 	{
 	for (i = 0; i < n - 1; i++)
-		printf("%d ",va_arg(ap, int));
+		printf("%d", va_arg(ap, int));
 	}
 	else
 	{
 	for (i = 0; i < n - 1; i++)
-		printf("%d%c " ,va_arg(ap, int) ,*separator);
+		printf("%d%s", va_arg(ap, int), separator);
 	}
-	printf("%d\n",va_arg(ap, int));
+
+	printf("%d\n", va_arg(ap, int));
 
 	va_end(ap);
 }
