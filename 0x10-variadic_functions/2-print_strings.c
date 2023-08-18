a3e4edc2048050fb3d8b9e2831c1765b0efb475e
octav8us <octav8us@gmail.com>
2023-08-18 03:15:36 +0300

    hotfix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,39 +13,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *param;
 
 	va_start(ap, n);
-	if (separator == NULL)
+	for (i = 0; i < n; i++)
 	{
-		for (i = 0; i < n - 1; i++)
-		{
-			param = va_arg(ap, char*);
-			if (param == NULL)
-			{
-				printf("%s ", "(nil)");
-				continue;
-			}
-			printf("%s ", param);
-		}
+		param = va_arg(ap, char *);
+		if (!param)
+			param = "(nil)";
+		if (!separator)
+			printf("%s", param);
+		else if (separator && i == 0)
+			printf("%s", param);
+		else
+			printf("%s%s", separator, param);
 	}
-	else
-	{
-		for (i = 0; i < n - 1; i++)
-		{
-			param = va_arg(ap, char*);
 
-			if (param == NULL)
-			{
-				printf("%s%s", "(nil)", separator);
-				continue;
-			}
-			printf("%s%s ", param, separator);
-		}
-	}
-	param = va_arg(ap, char*);
-	if (param == NULL)
-	{
-		printf("%s ", "(nil)");
-	}
-	else
-		printf("%s\n", param);
+	printf("\n");
+
 	va_end(ap);
 }
