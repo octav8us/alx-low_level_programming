4f05417cb0141a35fb92dcc3ba40bb2989aad1a0
octav8us <octav8us@gmail.com>
2023-08-29 17:46:29 +0300

    hotfix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,12 @@
 int pop_listint(listint_t **head)
 {
 	int n;
-	listint_t *temp = (*head)->next;
+	listint_t *temp;
 
 	if (!head || !*head)
 		return (0);
 	n = (*head)->n;
+	temp = (*head)->next;
 	free(*head);
 	*head = temp;
 
