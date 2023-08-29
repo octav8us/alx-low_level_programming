9ed262818af5dd515c69a12f709cb1c5dbab9ae0
octav8us <octav8us@gmail.com>
2023-08-29 13:18:00 +0300

    hotfix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -16,11 +16,13 @@ size_t print_listint(const listint_t *h)
 
 	size_t size = 0;
 
-	while (h->next != NULL)
+
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		size++;
-		h++;
+		h = h->next;
 	}
 	return (size);
 
